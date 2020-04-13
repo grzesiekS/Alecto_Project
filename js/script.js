ff469f9bef112b7f22036ec0379d75fb86813084
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-04-13 13:22:18 +0200

    Add js script for getting service-box section content

--- js/script.js
+++ js/script.js
@@ -4,21 +4,29 @@ const addServiceDropDownMenu = function() {
     console.log('Add service Drop Down Menu function');
 
     const popUpMenu = 'service-sub',
-        sectionService = 'our-services',
-        sectionServiceContainer = 'service-box',
-        sectionServiceDescription = 'description';
+        sectionService = '.our-services',
+        serviceBox = '.service-box',
+        serviceDescription = '.description';
 
     /* [DONE] Remove code in html for drop down menu in nav-bar section*/
 
     document.getElementById(popUpMenu).innerHTML = '';
 
     /* [IN PROGRESS] Get all  service names from our-service section*/
-    /* [IN PROGRESS] For all service-box in our-service section*/
+
+        const serviceElements = document.querySelector(sectionService);
+
+    /* [DONE] For all service-box in our-service section*/
+
+        const serviceBoxElements = serviceElements.querySelectorAll(serviceBox);
+
+        for(let element of serviceBoxElements) {
+            console.log('service-box: ', element);
 
         /* Get description conent */
 
         /* Get service name from h1 tag */
-
+        }
     /* create html code for drop down menu */
 
     /* Add html code to menu section for drop down menu */
