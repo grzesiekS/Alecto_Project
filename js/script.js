7af11115e802c6ab122f00b853dcba5add8cff7f
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-04-13 13:09:45 +0200

    Add js function addServiceDropDownMenu

---
+++ js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const addServiceDropDownMenu = function() {
+    console.log('Add service Drop Down Menu function');
+
+    const popUpMenu = 'service-sub',
+        sectionService = 'our-services',
+        sectionServiceContainer = 'service-box',
+        sectionServiceDescription = 'description';
+
+    /* [IN PROGRESS] Remove code in html for drop down menu in nav-bar section*/
+
+    /* Get all  service names from our-service section*/
+    /* For all service-box in our-service section*/
+
+        /* Get description conent */
+
+        /* Get service name from h1 tag */
+
+    /* create html code for drop down menu */
+
+    /* Add html code to menu section for drop down menu */
+}
+
+addServiceDropDownMenu();
